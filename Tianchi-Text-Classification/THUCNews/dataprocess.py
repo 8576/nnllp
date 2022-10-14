597456fb9864d5d7716ef1290c8949f275952ae8
8576 <xgzhangbing@126.com>
2022-10-14 21:42:36 +0800

    modify data process function

--- Tianchi-Text-Classification/THUCNews/dataprocess.py
+++ Tianchi-Text-Classification/THUCNews/dataprocess.py
@@ -1,15 +1,11 @@
 import pandas as pd
 import numpy as np
 import os
-from sklearn.utils import shuffle
 
 SEED = 100
-print(os.path.abspath("__file__"))
-print(os.listdir("THUCNews"))
 
+df = pd.read_csv(os.path.join("THUCNews", "data", "train_set.csv"), sep="\t")
 
-df = pd.read_csv(r"THUCNews\data\train_set.csv", sep="\t")
-# print(df)
 trainset = df.sample(frac=0.7, axis=0, random_state=SEED)
 for idx in range(10):
     print("=" * 10 + str(idx) + str(trainset.iloc[idx].label) +"=" * 10)
@@ -20,13 +16,13 @@ devset = df.sample(frac=0.63, axis=0, random_state=SEED)
 testset = df[~df.index.isin(devset.index)]
 print("=" * 5 + "trainset" + "=" * 5 )
 print(trainset, devset.__len__())
-# trainset.to_csv(os.path.join("THUCNews\data\\", "trainset.csv"), sep="\t", index=False)
+trainset.to_csv(os.path.join("THUCNews", "data", "trainset.csv"), sep="\t", index=False)
 print("=" * 5 + "devset" + "=" * 5 )
 print(devset, devset.__len__())
-# devset.to_csv(os.path.join("THUCNews\data\\", "devset.csv"), sep="\t", index=False)
+devset.to_csv(os.path.join("THUCNews", "data", "devset.csv"), sep="\t", index=False)
 print("=" * 5 + "testset" + "=" * 5 )
 print(testset, testset.__len__())
-# testset.to_csv(os.path.join("THUCNews\data\\", "testset.csv"), sep="\t", index=False)
+testset.to_csv(os.path.join("THUCNews", "data", "testset.csv"), sep="\t", index=False)
 
 print()
 # word_count = dict()
